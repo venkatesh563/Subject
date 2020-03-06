55c14e938d56f53d20b6d59edeca0016354099e9
Venkatesh Pattapu <venkatesh.pattapu@pbsystems.biz>
2020-03-06 17:10:02 +0530

    Sample

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { MessageService } from './../message.service';
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 
 @Component({
@@ -7,20 +7,26 @@ import { Component} from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  msg: any;
   constructor(public MessageService: MessageService) { }
 
   setMessage(event)
   {
+   
     //console.log(event.value);
 //this.MessageService.setMessageSubject(event.value);
 //this.MessageService.message.next(event.value);
 
-this.MessageService.Replymessage.subscribe(msg=>{
-  console.log('From 2nd sub:', msg);
-  })
+}
+ngOnInit()
+{
 
+  this.msg =  this.MessageService.Replymessage.subscribe(msg=>{
+
+    console.log('From 2nd sub:', msg);
+  
+    });
 }
 }
 
